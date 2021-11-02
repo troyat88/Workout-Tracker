33e07adc5596f73a503cc43953591b849320cace
Troy <troyat88@gmail.com>
2021-11-01 19:46:49 -0700

    deploying Heroku

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const apiRoutes = require("./routes/api")
  
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
